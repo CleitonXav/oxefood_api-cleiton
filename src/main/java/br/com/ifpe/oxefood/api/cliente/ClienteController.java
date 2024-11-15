a3c0ae3495b57efe1f3bec888080d9575d61eb6a
matheus3santos <mvsc7@discente.ifpe.edu.br>
2024-11-14 21:29:12 -0300

    Back-end: Implementando a Listagem C12

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
@@ -17,14 +22,28 @@ import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
 @CrossOrigin
 public class ClienteController {
 
-@Autowired
-private ClienteService clienteService;
+  @Autowired
+  private ClienteService clienteService;
 
-@PostMapping
-public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
+  @PostMapping
+  public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
 
-    Cliente clienteNovo = request.build();
-    Cliente cliente = clienteService.save(clienteNovo);
-    return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
-}
-}
+      Cliente clienteNovo = request.build();
+      Cliente cliente = clienteService.save(clienteNovo);
+      return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+  }
+     @GetMapping
+    public List<Cliente> listarTodos() {
+        return clienteService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterPorID(@PathVariable Long id) {
+        return clienteService.obterPorID(id);
+    }
+  }
+
+
+
+
+
