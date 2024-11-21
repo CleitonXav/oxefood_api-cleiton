10153bfa387566ab7c92f76f34b2d2793055460e
Cleiton <cleitonxav25@gmail.com>
2024-11-21 16:42:50 -0300

    Apresentando erro ... 44 common frames omitted ao roda o Back-end

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
@@ -7,15 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.PathVariable;
-
 import br.com.ifpe.oxefood.modelo.entregador.Entregador;
-import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
+import br.com.ifpe.oxefood.modelo.entregador.EntregadorRepository;
 
 @RestController
 @RequestMapping("/api/entregador")
@@ -23,7 +22,7 @@ import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
 public class EntregadorController {
 
   @Autowired
-  private EntregadorService entregadorService;
+  private EntregadorRepository entregadorService;
 
   @PostMapping
   public ResponseEntity<Entregador> save(@RequestBody EntregadorRequest request) {
@@ -42,3 +41,8 @@ public class EntregadorController {
         return entregadorService.obterPorID(id);
     }
   }
+
+
+
+
+
--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorRepository.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorRepository.java
@@ -1,7 +1,13 @@
 package br.com.ifpe.oxefood.modelo.entregador;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EntregadorRepository extends JpaRepository<Entregador, Long> {
+
+    List<Entregador> listarTodos();
+
+    Entregador obterPorID(Long id);
   
 }
