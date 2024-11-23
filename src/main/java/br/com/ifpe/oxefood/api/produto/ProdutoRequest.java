382cb926b491bff04fee3d19e32e0937ce2aaeb0
Cleiton <cleitonxav25@gmail.com>
2024-11-22 22:31:24 -0300

    Alteração do Back-end

--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
@@ -1,12 +1,8 @@
 package br.com.ifpe.oxefood.api.produto;
 
-import java.time.LocalDate;
+//import com.fasterxml.jackson.annotation.JsonFormat;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.produto.Produto;
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,28 +14,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProdutoRequest {
 
-   private String titulo;
+    private String titulo;
 
-   private String codigoDoProduto;
+    private String codigoDoProduto;
+ 
+    private String descrição;
+ 
+    private Double valorUnitario;
+ 
+    private Integer tempoDeEntregaMínimoEmMinutos;
+ 
+    private Integer tempoDeEntregaMáximoEmMinutos;
+ 
+   public Produto build() {
 
-   private String descrição;
-
-   private Double valorUnitario;
-
-   private Integer tempoDeEntregaMínimoEmMinutos;
-
-   private Integer tempoDeEntregaMáximoEmMinutos;
-
-  public Produto build() {
-
-      return Produto.builder()
-           .titulo(titulo)
-           .descrição(descrição)
-           .valorUnitario(valorUnitario)
-           .tempoDeEntregaMínimoEmMinutos(tempoDeEntregaMínimoEmMinutos)
-           .tempoDeEntregaMáximoEmMinutos(tempoDeEntregaMáximoEmMinutos)
-           .build();
+    return Produto.builder()
+    .titulo(titulo)
+    .descrição(descrição)
+    .valorUnitario(valorUnitario)
+    .tempoDeEntregaMínimoEmMinutos(tempoDeEntregaMínimoEmMinutos)
+    .tempoDeEntregaMáximoEmMinutos(tempoDeEntregaMáximoEmMinutos)
+    .build();
    }
-
-
 }
